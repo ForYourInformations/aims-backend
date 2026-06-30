0a6235db1dbc1b8115acff7f6636c2bdc9a1714b
sameh lakhdhar <lakhdhar.sameh@gmail.com>
2026-06-30 12:56:26 +0200

    Expliciter DEFAULT_PERMISSION_CLASSES

--- aimsea/settings.py
+++ aimsea/settings.py
@@ -79,6 +79,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
 }
 
 CSRF_TRUSTED_ORIGINS = [
