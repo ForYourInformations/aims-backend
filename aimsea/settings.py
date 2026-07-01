0f7f25e0d84f320e14f7ce157fb853ba08104a1d
sameh lakhdhar <lakhdhar.sameh@gmail.com>
2026-07-01 10:48:46 +0200

    Supprimer panneau Actions recentes de l'admin

--- aimsea/settings.py
+++ aimsea/settings.py
@@ -43,7 +43,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = "aimsea.urls"
 
 TEMPLATES = [{"BACKEND": "django.template.backends.django.DjangoTemplates",
-    "DIRS": [], "APP_DIRS": True, "OPTIONS": {"context_processors": [
+    "DIRS": [BASE_DIR / "templates"],
+    "APP_DIRS": True, "OPTIONS": {"context_processors": [
     "django.template.context_processors.debug",
     "django.template.context_processors.request",
     "django.contrib.auth.context_processors.auth",
